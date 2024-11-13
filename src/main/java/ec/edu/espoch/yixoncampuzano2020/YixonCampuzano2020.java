be4005c20ce7223d0c3fcbf9a8defc92f6863941
Stalyn0101 <yixon11@gmail.com>
2024-11-13 14:56:38 -0500

    hola

--- src/main/java/ec/edu/espoch/yixoncampuzano2020/YixonCampuzano2020.java
+++ src/main/java/ec/edu/espoch/yixoncampuzano2020/YixonCampuzano2020.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ec.edu.espoch.yixoncampuzano2020;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,39 @@ package ec.edu.espoch.yixoncampuzano2020;
 public class YixonCampuzano2020 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        float[] ventas = new float[7];
+        float totalVentas = 0;
+        float ventasDiarias=0;
+
+        int i = 0;
+
+        do {
+            System.out.println("Ingrese el monto de la venta");
+            totalVentas = scanner.nextFloat();
+            if (totalVentas > 0) {
+                ventas[i] = totalVentas;
+                i++;
+
+            } else {
+                System.out.println("El valor ingresado debe ser positivo");
+            }
+        } while (i < 7);
+        // Menu principal
+        System.out.println("Calcular total ventas");
+        System.out.println("Calcular el dia con mas ventas");
+        System.out.println("Mostrar el dia con venta mas alta");
+        System.out.println("Calcular monto acumulado");
+        System.out.println("Salir");
+
+        int menu = scanner.nextInt();
+        switch (menu) {
+            case 1:
+                System.out.println("El total de la venta de la semana"+totalVentas);
+                break;
+            case 2:
+                
+                 
     }
 }
+    }
