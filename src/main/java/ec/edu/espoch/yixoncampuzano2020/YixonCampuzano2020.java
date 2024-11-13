88bf2d9c9da3266b9247e1fe0514964d20d4b7c5
Stalyn0101 <yixon11@gmail.com>
2024-11-13 13:16:33 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/yixoncampuzano2020/YixonCampuzano2020.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.yixoncampuzano2020;
+
+/**
+ *
+ * @author USER
+ */
+public class YixonCampuzano2020 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
